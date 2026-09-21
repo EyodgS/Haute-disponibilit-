11df2907b6871789a46f0fa69b013c8d6f5788f8
EyodgS <eliodages@gmail.com>
2026-09-21 09:46:08 +0200

    Phase 1 : nginx+php-fpm sur WEB1/WEB2, sonde /health, mesure du SPOF (RTO observé : 3.4 s)

--- configs/web2/www/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = gethostname();
-header('Content-Type: application/json');
-echo json_encode([
-    'status' => 'ok',
-    'node' => $hostname,
-    'service' => 'tp-ha-web',
-], JSON_PRETTY_PRINT);
